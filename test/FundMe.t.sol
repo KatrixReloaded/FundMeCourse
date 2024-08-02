1b65b3ac8595c2b78e497a8bc7fc54cfb8b948e2
KatrixReloaded <kartikeyasureka@gmail.com>
2024-08-02 15:52:13 +0530

    test functions added

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -7,6 +7,11 @@ import {DeployFundMe} from "../script/FundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+
+    address tester = makeAddr("tester"); // generating a tester/user address
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         DeployFundMe dfundMe = new DeployFundMe();
         fundMe = dfundMe.run();
@@ -25,7 +30,16 @@ contract FundMeTest is Test {
         assertEq(version, 4);
     }
 
-    function testFundFailsWithoutEnougEth() public {
+    function testFundFailsWithoutEnoughEth() public {
         vm.expectRevert(); // the next line should revert
+        fundMe.fund(); // 0 value, less than MINIMUM_USD ($5)
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.deal(tester, STARTING_BALANCE);
+        vm.prank(tester);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(tester);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
